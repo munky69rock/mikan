f510bd727f9a19cf914aa448c8e65eb00fb6b362
Masayuki Uehara <munky69rock@gmail.com>
2017-06-27 12:23:58 +0900

    Removed 404 resources

--- cron/esa.js
+++ cron/esa.js
@@ -11,7 +11,6 @@ const images = [
   'https://pbs.twimg.com/media/BJZU7q4CIAEL7Vp.jpg:small',
   'https://sdl-stickershop.line.naver.jp/stickershop/v1/product/1015196/iphone/main@2x.png',
   'http://f.maruyu.catchball.co/img/jo9.png',
-  'http://65.media.tumblr.com/c79d6b59dc4ee10595564c8aafc2225e/tumblr_mrkrngeogp1sckns5o1_500.png',
   'http://tn-skr3.smilevideo.jp/smile?i=24187254.L',
   'http://livedoor.blogimg.jp/tank_make/imgs/3/b/3b089b3a.jpg',
   'http://66.media.tumblr.com/067d04f3df4605af4a6c574237cb06e2/tumblr_mqlmq4YufD1sckns5o1_500.jpg',
