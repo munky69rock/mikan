b793087f2859e3fe25fc9c9b4d2f962f117562a8
Masayuki Uehara <munky69rock@gmail.com>
2017-06-27 12:23:44 +0900

    Upgraded

---
+++ scripts/shuffle.js
@@ -0,0 +1,12 @@
+// Commands:
+//  shuffle - Choose users
+
+const _ = require('lodash');
+
+module.exports = controller => {
+  controller.hears([/shuffle (.*)/i], 'direct_message,direct_mention,mention', (bot, message) => {
+    const list = RegExp.$1.split(/[\s　]/);
+    bot.reply(message, _.shuffle(list).map((n, i) => `${i + 1}. ${n}`).join("\n"));
+  });
+};
+
